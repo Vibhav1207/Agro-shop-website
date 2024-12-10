c02731324accc06e9d57968f8c13478e6446f5d1
Vibahv Patel <77201529+Vibhav1207@users.noreply.github.com>
2024-12-10 18:36:24 +0530

    a

--- components/Footer.jsx
+++
@@ -1,8 +0,0 @@
-const Footer = () => (
-    <footer style={{ textAlign: "center", padding: "1em 0", color: "white" }}>
-      <p>© 2024 Agro Shop. All Rights Reserved.</p>
-    </footer>
-  );
-  
-  export default Footer;
-  
